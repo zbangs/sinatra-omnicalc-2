f4a03a8076c1074ccdca919509f4bb2294cc6657
Zackery Bangs <106060493+zbangs@users.noreply.github.com>
2024-12-22 01:09:12 +0000

    Refactor and resolve errors

--- app.rb
+++ app.rb
@@ -5,17 +5,22 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+
+# Add
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
 end
 
+
+# Subtract
 get("/subtract") do
   erb(:sub_form)
 end
@@ -24,21 +29,34 @@ get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
 
+  @result =  @second_num - @first_num
   erb(:sub_result)
 end
 
+
+# Multiply
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num * @second_num
   erb(:mult_result)
 end
 
+
+# Divide
 get("/divide") do
   erb(:div_form)
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num / @second_num
   erb(:div_result)
 end
